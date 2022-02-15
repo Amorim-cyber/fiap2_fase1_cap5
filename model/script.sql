b5d96c9957b23208bbf5f22688a81d99d6b9ada5
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-02-15 15:02:06 -0300

    Atualiza model/script.sql [2]

--- model/script.sql
+++ model/script.sql
@@ -1,12 +1,14 @@
 -- DELETAR HISTORICO (COMPUTE CASO AS ESTRURAS JA EXISTAM NO BANCO) --
 
 drop table tb_condominio cascade constraints;
+drop table tb_morada cascade constraints;
 drop table tb_morador cascade constraints;
 drop table tb_prestador cascade constraints;
-drop table tb_registro_condominio cascade constraints;
+drop table tb_registro_morada cascade constraints;
 drop table tb_registro_servico cascade constraints;
 drop table tb_servico cascade constraints;
 drop sequence sq_tb_condominio;
+drop sequence sq_tb_morada;
 drop sequence sq_tb_morador;
 drop sequence sq_tb_prestador;
 drop sequence sq_tb_registro;
@@ -16,6 +18,7 @@ drop sequence sq_tb_servico;
 -- CRIAÇÃO DE SEQUENCES --
 
 create sequence sq_tb_condominio start with 1 increment by 1;
+create sequence sq_tb_morada start with 1 increment by 1;
 create sequence sq_tb_morador start with 1 increment by 1;
 create sequence sq_tb_prestador start with 1 increment by 1;
 create sequence sq_tb_registro start with 1 increment by 1;
@@ -32,10 +35,18 @@ CREATE TABLE tb_condominio (
 
 ALTER TABLE tb_condominio ADD CONSTRAINT tb_condominio_pk PRIMARY KEY ( id_condominio );
 
+CREATE TABLE tb_morada (
+    id_morada                    NUMBER(10) NOT NULL,
+    nr_morada                    NUMBER(10) NOT NULL,
+    tipo_morada                  VARCHAR2(20) NOT NULL,
+    tb_condominio_id_condominio  NUMBER(10) NOT NULL
+);
+
+ALTER TABLE tb_morada ADD CONSTRAINT tb_morada_pk PRIMARY KEY ( id_morada );
+
 CREATE TABLE tb_morador (
     id_morador  NUMBER(10) NOT NULL,
-    nm_morador  VARCHAR2(100) NOT NULL,
-    nr_morador  NUMBER(19) NOT NULL
+    nm_morador  VARCHAR2(100) NOT NULL
 );
 
 ALTER TABLE tb_morador ADD CONSTRAINT tb_morador_pk PRIMARY KEY ( id_morador );
@@ -48,19 +59,19 @@ CREATE TABLE tb_prestador (
 
 ALTER TABLE tb_prestador ADD CONSTRAINT tb_prestador_pk PRIMARY KEY ( id_prestador );
 
-CREATE TABLE tb_registro_condominio (
-    tb_condominio_id_condominio  NUMBER(10) NOT NULL,
-    tb_morador_id_morador        NUMBER(10) NOT NULL
+CREATE TABLE tb_registro_morada (
+    tb_morador_id_morador  NUMBER(10) NOT NULL,
+    tb_morada_id_morada    NUMBER(10) NOT NULL
 );
 
 CREATE TABLE tb_registro_servico (
     id_registro                NUMBER(10) NOT NULL,
     dt_inicio                  TIMESTAMP WITH LOCAL TIME ZONE NOT NULL,
     dt_termino                 TIMESTAMP WITH LOCAL TIME ZONE NOT NULL,
-    status                     VARCHAR2(15) NOT NULL,
     tb_prestador_id_prestador  NUMBER(10) NOT NULL,
     tb_morador_id_morador      NUMBER(10) NOT NULL,
-    tb_servico_id_servico      NUMBER(10) NOT NULL
+    tb_servico_id_servico      NUMBER(10) NOT NULL,
+    status                     VARCHAR2(15) NOT NULL
 );
 
 ALTER TABLE tb_registro_servico ADD CONSTRAINT tb_registro_servico_pk PRIMARY KEY ( id_registro );
@@ -74,20 +85,24 @@ ALTER TABLE tb_servico ADD CONSTRAINT tb_servico_pk PRIMARY KEY ( id_servico );
 
 --  CHAVES ESTRANGEIRAS  --
 
-ALTER TABLE tb_registro_condominio
-    ADD CONSTRAINT tb_condominio_fk FOREIGN KEY ( tb_condominio_id_condominio )
+ALTER TABLE tb_morada
+    ADD CONSTRAINT mor_condo_fk FOREIGN KEY ( tb_condominio_id_condominio )
         REFERENCES tb_condominio ( id_condominio );
 
-ALTER TABLE tb_registro_condominio
-    ADD CONSTRAINT regis_condo_mor_fk FOREIGN KEY ( tb_morador_id_morador )
+ALTER TABLE tb_registro_morada
+    ADD CONSTRAINT regis_mor_fk FOREIGN KEY ( tb_morada_id_morada )
+        REFERENCES tb_morada ( id_morada );
+        
+ALTER TABLE tb_registro_morada
+    ADD CONSTRAINT regis_mora_fk FOREIGN KEY ( tb_morador_id_morador )
         REFERENCES tb_morador ( id_morador );
 
 ALTER TABLE tb_registro_servico
-    ADD CONSTRAINT regis_serv_mor_fk FOREIGN KEY ( tb_morador_id_morador )
+    ADD CONSTRAINT regis_serv_mora_fk FOREIGN KEY ( tb_morador_id_morador )
         REFERENCES tb_morador ( id_morador );
 
 ALTER TABLE tb_registro_servico
-    ADD CONSTRAINT regis_serv_prest_fk FOREIGN KEY ( tb_prestador_id_prestador )
+    ADD CONSTRAINT regis_serv_pres_fk FOREIGN KEY ( tb_prestador_id_prestador )
         REFERENCES tb_prestador ( id_prestador );
 
 ALTER TABLE tb_registro_servico
