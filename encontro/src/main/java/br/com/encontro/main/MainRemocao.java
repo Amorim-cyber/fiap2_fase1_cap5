f33c5071cc433eab9d44e9847959b2bea2d00e3e
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-02-16 11:51:53 -0300

    Adiciona MainRemocao.java

---
+++ encontro/src/main/java/br/com/encontro/main/MainRemocao.java
@@ -0,0 +1,32 @@
+package br.com.encontro.main;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import br.com.encontro.entity.Servico;
+
+public class MainRemocao {
+
+	public static void main(String[] args) {
+		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("encontro");
+		EntityManager em = fabrica.createEntityManager();
+		
+		Servico servico = em.find(Servico.class, 1);
+		
+		try {
+			em.remove(servico);
+			em.getTransaction().begin();
+			em.getTransaction().commit();
+			
+		}catch(Exception e) {
+			if(em.getTransaction().isActive()) {
+				em.getTransaction().rollback();
+			}
+		}
+		
+		em.close();
+		fabrica.close();
+		
+	}
+
+}
